705c649b797d66ddd27cbbc7c44011558b0c760b
Something <peyton.bowe72@gmail.com>
2020-03-29 19:15:04 -0500

    Initial push

---
+++ __resource.lua
@@ -0,0 +1,5 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+client_script 'reditor_c.lua'
+
+--Using the deprecated meathod because I can't figure out why fxmanifest won't start the script ¯\_(ツ)_/¯
---
+++ reditor_c.lua
@@ -0,0 +1,21 @@
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		if IsControlPressed(1, 288) then --288 = F1 if you want to change the keybinds to something else see https://docs.fivem.net/docs/game-references/controls/
+			StartRecording(1) 
+		end
+		if IsControlPressed(1, 289) then -- 289 = F2 if you want to change the keybinds to something else see https://docs.fivem.net/docs/game-references/controls/
+			StopRecordingAndSaveClip()
+		end
+		if IsControlPressed(1, 170) then -- 170 = F3 if you want to change the keybinds to something else see https://docs.fivem.net/docs/game-references/controls/
+			NetworkSessionEnd(true, true) --End the current session (required if you want R* editor to work)
+			ActivateRockstarEditor()
+			while IsPauseMenuActive() do 
+				Citizen.Wait(0)
+				DoScreenFadeIn(1)
+				NetworkSessionHost(-1, 32, false) --Attempts reconnection to the session after exiting the editor
+			end
+		end
+	end
+end)
+
