c106e139c767c50b0a8155dfb32dd4ad722b3de9
Something <peyton.bowe72@gmail.com>
2020-04-12 12:24:32 -0500

    Added comments

--- reditor.js
+++ reditor.js
@@ -1,21 +1,21 @@
 setTick(() => {
-    if(IsControlPressed(1, 288)) {
+    if(IsControlPressed(1, 288)) { //F1 if you want to change the keybinds to something else see https://docs.fivem.net/docs/game-references/controls/
         StartRecording(1);
     }
-    if(IsControlPressed(1, 289)) {
+    if(IsControlPressed(1, 289)) { //F2 if you want to change the keybinds to something else see https://docs.fivem.net/docs/game-references/controls/
         StopRecordingAndSaveClip();
     }
-    if(IsControlPressed(1, 170)) {
-        NetworkSessionEnd(true, true);
-        ActivateRockstarEditor();
-        setTimeout(wait, 1);
+    if(IsControlPressed(1, 170)) { //F3 if you want to change the keybinds to something else see https://docs.fivem.net/docs/game-references/controls/
+        NetworkSessionEnd(true, true); //End the current session (required if you want R* editor to work) 
+        ActivateRockstarEditor(); //Activates R* editor
+        setTimeout(wait, 1); //Calls wait function and waits one millisecond 
     }
 });
 
 function wait() {
     do {
-        DoScreenFadeIn(1);
-        NetworkSessionHost(-1, 32, false);
+        DoScreenFadeIn(1); 
+        NetworkSessionHost(-1, 32, false); //Attempts reconnection to the session after exiting the editor
     }
-    while(IsPauseMenuActive());
+    while(IsPauseMenuActive()); //If the pause menu is active do above
 }
--- reditor_c.lua
+++ reditor_c.lua
@@ -9,8 +9,8 @@ Citizen.CreateThread(function()
 		end
 		if IsControlPressed(1, 170) then -- 170 = F3 if you want to change the keybinds to something else see https://docs.fivem.net/docs/game-references/controls/
 			NetworkSessionEnd(true, true) --End the current session (required if you want R* editor to work)
-			ActivateRockstarEditor()
-			while IsPauseMenuActive() do 
+			ActivateRockstarEditor() --Activates R* editor
+			while IsPauseMenuActive() do --If the pause menu is active do below
 				Citizen.Wait(0)
 				DoScreenFadeIn(1)
 				NetworkSessionHost(-1, 32, false) --Attempts reconnection to the session after exiting the editor
